49c0d5ec8e2526a3c00974c4f01b7760a37e38c3
Rodrigo-Antonio-Fernandes <56327258+Rodrigo-Antonio-Fernandes@users.noreply.github.com>
2020-03-11 20:12:19 -0300

    Add files via upload

---
+++ Rodrigo/Rodrigo/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Rodrigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.TxtCaixa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insira o valor";
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(15, 51);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(45, 23);
+            this.btnSoma.TabIndex = 1;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(66, 51);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(45, 23);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(117, 51);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(45, 23);
+            this.btnMult.TabIndex = 1;
+            this.btnMult.Text = "*";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(167, 51);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(45, 23);
+            this.btnDiv.TabIndex = 1;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(15, 80);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(199, 23);
+            this.btnResult.TabIndex = 1;
+            this.btnResult.Text = "Resultado";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // TxtCaixa
+            // 
+            this.TxtCaixa.Location = new System.Drawing.Point(15, 25);
+            this.TxtCaixa.Name = "TxtCaixa";
+            this.TxtCaixa.Size = new System.Drawing.Size(197, 20);
+            this.TxtCaixa.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 122);
+            this.Controls.Add(this.TxtCaixa);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.TextBox TxtCaixa;
+    }
+}
+
---
+++ Rodrigo/Rodrigo/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rodrigo
+{
+    public partial class Form1 : Form
+    {
+		int val1, val2, Result;
+		string Op;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+			if (Op == null)
+			{
+				val1 = Convert.ToInt16(TxtCaixa.Text);
+				Result = val1;
+				TxtCaixa.Text = "";
+				Op = "*";
+			}
+			else
+			{
+				if (Op == "*")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result * val2;
+					TxtCaixa.Text = "";
+					Op = "*";
+				}
+				if (Op == "-")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result - val2;
+					TxtCaixa.Text = "";
+					Op = "*";
+				}
+				if (Op == "+")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result + val2;
+					TxtCaixa.Text = "";
+					Op = "*";
+				}
+				if (Op == "/")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result / val2;
+					TxtCaixa.Text = "";
+					Op = "*";
+				}
+			}
+		}
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+			if (Op == null)
+			{
+				val1 = Convert.ToInt16(TxtCaixa.Text);
+				Result = val1;
+				TxtCaixa.Text = "";
+				Op = "+";
+			}
+			else
+			{
+				if (Op == "+")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result + val2;
+					TxtCaixa.Text = "";
+				}
+				if (Op == "-")
+				{
+					
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result - val2;
+					TxtCaixa.Text = "";
+					Op = "+";
+				}
+				if (Op == "*")
+				{
+					
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result * val2;
+					TxtCaixa.Text = "";
+					Op = "+";
+				}
+				if (Op == "/")
+				{					
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result / val2;
+					TxtCaixa.Text = "";
+					Op = "+";
+				}
+			}
+		}
+
+		private void btnResult_Click(object sender, EventArgs e)
+		{
+			if (Op == "/")
+			{
+				val2 = Convert.ToInt16(TxtCaixa.Text);
+				Result = Result / val2;
+				TxtCaixa.Text = Convert.ToString(Result);
+			}
+			if (Op == "*")
+			{
+				val2 = Convert.ToInt16(TxtCaixa.Text);
+				Result = Result * val2;
+				TxtCaixa.Text = Convert.ToString(Result);
+			}
+			if (Op == "+")
+			{
+				val2 = Convert.ToInt16(TxtCaixa.Text);
+				Result = Result + val2;
+				TxtCaixa.Text = Convert.ToString(Result);
+			}
+			if (Op == "-")
+			{
+				val2 = Convert.ToInt16(TxtCaixa.Text);
+				Result = Result - val2;
+				TxtCaixa.Text = Convert.ToString(Result);
+			}
+		}
+
+		private void btnSub_Click(object sender, EventArgs e)
+		{
+			if (Op == null)
+			{
+				val1 = Convert.ToInt16(TxtCaixa.Text);
+				Result = val1;
+				TxtCaixa.Text = "";
+				Op = "-";
+			}
+			else
+			{
+				if (Op == "-")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result - val2;
+					TxtCaixa.Text = "";
+				}
+				if (Op == "+")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result + val2;
+					TxtCaixa.Text = "";
+					Op = "-";
+				}
+				if (Op == "*")
+				{
+					
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result * val2;
+					TxtCaixa.Text = "";
+					Op = "-";
+				}
+				if (Op == "/")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result / val2;
+					TxtCaixa.Text = "";
+					Op = "-";
+				}
+			}
+		}
+
+		private void btnDiv_Click(object sender, EventArgs e)
+		{
+			if (Op == null)
+			{
+				val1 = Convert.ToInt16(TxtCaixa.Text);
+				Result = val1;
+				TxtCaixa.Text = "";
+				Op = "/";
+			}
+			else
+			{
+				if (Op == "/")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result / val2;
+					TxtCaixa.Text = "";
+					Op = "/";
+				}
+				if (Op == "-")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result - val2;
+					TxtCaixa.Text = "";
+					Op = "/";
+				}
+				if (Op == "+")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result + val2;
+					TxtCaixa.Text = "";
+					Op = "/";
+				}
+				if (Op == "*")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					Result = Result * val2;
+					TxtCaixa.Text = "";
+					Op = "/";
+				}
+			}
+		}
+	}
+}
