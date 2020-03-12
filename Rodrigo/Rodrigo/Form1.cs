cbb0464de50be8d64016bf40fa50aebb31b9ca0a
Rodrigo-Antonio-Fernandes <56327258+Rodrigo-Antonio-Fernandes@users.noreply.github.com>
2020-03-11 22:17:02 -0300

    Add files via upload

--- Rodrigo/Rodrigo/Form1.cs
+++ Rodrigo/Rodrigo/Form1.cs
@@ -17,203 +17,250 @@ namespace Rodrigo
         public Form1()
         {
             InitializeComponent();
-        }
+		}
 
         private void button3_Click(object sender, EventArgs e)
         {
-			if (Op == null)
+			try
 			{
-				val1 = Convert.ToInt16(TxtCaixa.Text);
-				Result = val1;
-				TxtCaixa.Text = "";
-				Op = "*";
+				if (Op == null)
+				{
+					val1 = Convert.ToInt16(TxtCaixa.Text);
+					Result = val1;
+					TxtCaixa.Text = "";
+					TxtCaixa.Focus();
+					Op = "*";
+				}
+				else
+				{
+					if (Op == "*")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result * val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "*";
+					}
+					if (Op == "-")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result - val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "*";
+					}
+					if (Op == "+")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result + val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "*";
+					}
+					if (Op == "/")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result / val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "*";
+					}
+				}
 			}
-			else
+			catch (Exception)
 			{
-				if (Op == "*")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result * val2;
-					TxtCaixa.Text = "";
-					Op = "*";
-				}
-				if (Op == "-")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result - val2;
-					TxtCaixa.Text = "";
-					Op = "*";
-				}
-				if (Op == "+")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result + val2;
-					TxtCaixa.Text = "";
-					Op = "*";
-				}
-				if (Op == "/")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result / val2;
-					TxtCaixa.Text = "";
-					Op = "*";
-				}
+				MessageBox.Show("Valor incorreto"); ;
 			}
+			
 		}
 
         private void btnSoma_Click(object sender, EventArgs e)
         {
-			if (Op == null)
+			try
 			{
-				val1 = Convert.ToInt16(TxtCaixa.Text);
-				Result = val1;
-				TxtCaixa.Text = "";
-				Op = "+";
+				if (Op == null)
+				{
+					val1 = Convert.ToInt16(TxtCaixa.Text);
+					Result = val1;
+					TxtCaixa.Text = "";
+					TxtCaixa.Focus();
+					Op = "+";
+				}
+				else
+				{
+					if (Op == "+")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result + val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+					}
+					if (Op == "-")
+					{
+
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result - val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "+";
+					}
+					if (Op == "*")
+					{
+
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result * val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "+";
+					}
+					if (Op == "/")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result / val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "+";
+					}
+				}
 			}
-			else
+			catch (Exception)
 			{
-				if (Op == "+")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result + val2;
-					TxtCaixa.Text = "";
-				}
-				if (Op == "-")
-				{
-					
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result - val2;
-					TxtCaixa.Text = "";
-					Op = "+";
-				}
-				if (Op == "*")
-				{
-					
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result * val2;
-					TxtCaixa.Text = "";
-					Op = "+";
-				}
-				if (Op == "/")
-				{					
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result / val2;
-					TxtCaixa.Text = "";
-					Op = "+";
-				}
+				MessageBox.Show("Valor incorreto");
 			}
+			
 		}
 
 		private void btnResult_Click(object sender, EventArgs e)
-		{
-			if (Op == "/")
-			{
-				val2 = Convert.ToInt16(TxtCaixa.Text);
-				Result = Result / val2;
-				TxtCaixa.Text = Convert.ToString(Result);
-			}
-			if (Op == "*")
-			{
-				val2 = Convert.ToInt16(TxtCaixa.Text);
-				Result = Result * val2;
-				TxtCaixa.Text = Convert.ToString(Result);
-			}
-			if (Op == "+")
-			{
-				val2 = Convert.ToInt16(TxtCaixa.Text);
-				Result = Result + val2;
-				TxtCaixa.Text = Convert.ToString(Result);
-			}
-			if (Op == "-")
-			{
-				val2 = Convert.ToInt16(TxtCaixa.Text);
-				Result = Result - val2;
-				TxtCaixa.Text = Convert.ToString(Result);
-			}
+		{			
+				if (Op == "/")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					TxtCaixa.Text = Convert.ToString(Result / val2);
+				}
+				if (Op == "*")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					TxtCaixa.Text = Convert.ToString(Result * val2);
+				}
+				if (Op == "+")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					TxtCaixa.Text = Convert.ToString(Result + val2);
+				}
+				if (Op == "-")
+				{
+					val2 = Convert.ToInt16(TxtCaixa.Text);
+					TxtCaixa.Text = Convert.ToString(Result - val2);
+				}						
 		}
 
 		private void btnSub_Click(object sender, EventArgs e)
 		{
-			if (Op == null)
+			try
 			{
-				val1 = Convert.ToInt16(TxtCaixa.Text);
-				Result = val1;
-				TxtCaixa.Text = "";
-				Op = "-";
+				if (Op == null)
+				{
+					val1 = Convert.ToInt16(TxtCaixa.Text);
+					Result = val1;
+					TxtCaixa.Text = "";
+					TxtCaixa.Focus();
+					Op = "-";
+				}
+				else
+				{
+					if (Op == "-")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result - val2;
+						TxtCaixa.Focus();
+						TxtCaixa.Text = "";
+					}
+					if (Op == "+")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result + val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "-";
+					}
+					if (Op == "*")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result * val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "-";
+					}
+					if (Op == "/")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result / val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "-";
+					}
+				}
 			}
-			else
+			catch (Exception)
 			{
-				if (Op == "-")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result - val2;
-					TxtCaixa.Text = "";
-				}
-				if (Op == "+")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result + val2;
-					TxtCaixa.Text = "";
-					Op = "-";
-				}
-				if (Op == "*")
-				{
-					
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result * val2;
-					TxtCaixa.Text = "";
-					Op = "-";
-				}
-				if (Op == "/")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result / val2;
-					TxtCaixa.Text = "";
-					Op = "-";
-				}
+
+				MessageBox.Show("Valor incorreto");
 			}
+			
 		}
 
 		private void btnDiv_Click(object sender, EventArgs e)
 		{
-			if (Op == null)
+			try
 			{
-				val1 = Convert.ToInt16(TxtCaixa.Text);
-				Result = val1;
-				TxtCaixa.Text = "";
-				Op = "/";
+				if (Op == null)
+				{
+					val1 = Convert.ToInt16(TxtCaixa.Text);
+					Result = val1;
+					TxtCaixa.Text = "";
+					TxtCaixa.Focus();
+					Op = "/";
+				}
+				else
+				{
+					if (Op == "/")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result / val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "/";
+					}
+					if (Op == "-")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result - val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "/";
+					}
+					if (Op == "+")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result + val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "/";
+					}
+					if (Op == "*")
+					{
+						val2 = Convert.ToInt16(TxtCaixa.Text);
+						Result = Result * val2;
+						TxtCaixa.Text = "";
+						TxtCaixa.Focus();
+						Op = "/";
+					}
+				}
 			}
-			else
+			catch (Exception)
 			{
-				if (Op == "/")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result / val2;
-					TxtCaixa.Text = "";
-					Op = "/";
-				}
-				if (Op == "-")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result - val2;
-					TxtCaixa.Text = "";
-					Op = "/";
-				}
-				if (Op == "+")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result + val2;
-					TxtCaixa.Text = "";
-					Op = "/";
-				}
-				if (Op == "*")
-				{
-					val2 = Convert.ToInt16(TxtCaixa.Text);
-					Result = Result * val2;
-					TxtCaixa.Text = "";
-					Op = "/";
-				}
-			}
+				MessageBox.Show("Valor incorreto");
+			}			
 		}
 	}
 }
